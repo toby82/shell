8e28e47dfb51da5868d89316570c13b924b5138a
zhangjianhong <zhangjianhong@chinacloud.com.cn>
2016-03-25 10:55:18 +0800

    modified:   config.py

--- config.py
+++ config.py
@@ -24,11 +24,11 @@ def get_allnode():
     return allnode  
 def get_allhostnameip():
     local = salt.client.LocalClient()
-    online_hostnameip = dict(local.cmd('*','grains.get',['ip_interfaces:eth0']))
+    online_hostnameip = dict(local.cmd('*','grains.get',['ipv4']))
     hostnameip = {}
     for k,v in online_hostnameip.items():
-        hostnameip[k] = v[0]
-    return hostnameip     
+        hostnameip[k] = v[1]
+    return hostnameip   
 def get_cchostname():
     local = salt.client.LocalClient()
     online_host = dict(local.cmd('*','test.ping'))
@@ -64,7 +64,7 @@ def set_st_nw():
     if config['storage_network']:
         for host in config['st_nw'].keys():
             if host not in get_allhostnameip().keys():
-                print "Please check config.sls [st_nw]...hostname"
+                print "ERROR:  check config.sls [st_nw]...hostname"
                 sys.exit(1)
     else:
         del config['st_nw']
@@ -73,6 +73,38 @@ def gluster_server_hostname():
     for host in config['glusterfs']['nodes']:
         if 'server' in host.values()[0].values():
             return host.keys()[0]
+def get_all_gluster_server():
+    try:
+        gluster_server_hostname = []
+        for host in config['glusterfs']['nodes']:
+            for roles in host.values():
+                for role in roles.values():
+                    if role == 'server':
+                        gluster_server_hostname.append(host.keys()[0])
+        return gluster_server_hostname
+    except:
+        print "ERROR check [glusterfs]"
+        sys.exit(1)
+def get_vmw_agent():
+    try:
+        vmw_agent = config['iaas_role']['vmw_agent']
+    except:
+        print "ERROR check ['iaas_role']->vmw_agent"
+        sys.exit(1)
+    if vmw_agent in get_all_gluster_server():
+        print "ERROR vmw_agent Can't appear in the gluster server"
+        sys.exit(1)
+    else:
+        return vmw_agent
+       
+def get_gluster_server_count():
+    count = 0
+    for i in range(len(config['glusterfs']['nodes'])):
+        for k in config['glusterfs']['nodes'][i].keys():
+            for v in config['glusterfs']['nodes'][i][k]:
+                if config['glusterfs']['nodes'][i][k][v] == 'server':
+                    count +=1
+    return count
 def gluster_server_mg_nw():
     return get_allhostnameip()[gluster_server_hostname()]
 def st_nw_info():
@@ -87,10 +119,10 @@ def get_allinone_hypervisor():
         allinone_hypervisor = []
         allinone_hypervisor = config['allinone_hypervisor'].split(',')
     except:
-        print "Please check [allinone_hypervisor]"
+        print "ERROR:  check [allinone_hypervisor]"
         sys.exit(1)
     return allinone_hypervisor
-
+   
 def set_storage_type():
     if config['storage_type'] in ['local','gluster','ceph','ocfs2']:
         if config['storage_type'] == 'local':
@@ -106,7 +138,7 @@ def set_storage_type():
                 lv_size = config['cinder_info']['lvm_volumes_size']
                 m = re.search(r'[^0]\d+G', lv_size)
             except:
-                print "Please check lvm_volumes_size."
+                print "ERROR:  check lvm_volumes_size."
                 sys.exit(1)
             if m:
                 pass
@@ -121,13 +153,19 @@ def set_storage_type():
             config['glusterfs']['enable'] = True
             config['cinder_info']['backend'] = 'gluster'
             config['cinder_info']['lvm_enable'] = 'n'
+            if get_gluster_server_count() != 0 or get_gluster_server_count() % 2 == 0:
+                config['glusterfs']['replica'] = 2
+            else:
+                config['glusterfs']['replica'] = 0
             try:
                 if config['storage_network']:
+                    config['glusterfs']['network'] = 'st_nw'
                     config['cinder_info']['gluster_mounts'] = gluster_st_ip() + ':/cinder-vol'
                 else:
                     config['cinder_info']['gluster_mounts'] = gluster_server_mg_nw() + ':/cinder-vol'
+                    config['glusterfs']['network'] = 'mg_nw'
             except:
-                print "Please check config.sls [glusterfs]"
+                print "ERROR:  check config.sls [glusterfs]"
                 sys.exit(1)
         elif config['storage_type'] == 'ceph':
             print config['storage_type'] + ' storage'
@@ -143,7 +181,7 @@ def set_storage_type():
                                     'lvm_enable':'n'}
         elif config['storage_type'] == 'ocfs2':
             if config['lun_info']['enable'] == False or config['ocfs2_cluster']['enable'] == False:
-                print "Please check config.sls [lun_info] and [ocfs2_cluster]"
+                print "ERROR:  check config.sls [lun_info] and [ocfs2_cluster]"
                 sys.exit(1)
             print config['storage_type'] + ' storage'
             del config['nova_info']
@@ -155,9 +193,9 @@ def set_storage_type():
                                     'lvm_enable':'n'}
             
     else:
-        print "Please check storage type in config.sls:[local,gluster,ceph,ocfs2]"
+        print "ERROR:  check storage type in config.sls:[local,gluster,ceph,ocfs2]"
         sys.exit(1)
-        
+      
 #main    
 if config['allinone_enable']:
     get_set = set(get_allinone_hypervisor())
@@ -214,10 +252,10 @@ if config['allinone_enable']:
             config['ironic_info']['install'] = 'y'
             config['iaas_role']['vmw_agent'] = get_cchostname()
         else:
-            print "Please check hypervisor allinone_hypervisor in config.sls:[kvm,vmware,ironic]"
+            print "ERROR:  check hypervisor allinone_hypervisor in config.sls:[kvm,vmware,ironic]"
             sys.exit(1)
     else:
-        print "Please check hypervisor allinone_hypervisor in config.sls:[kvm,vmware,ironic]"
+        print "ERROR:  check hypervisor allinone_hypervisor in config.sls:[kvm,vmware,ironic]"
         sys.exit(1)
     if get_st_type() in ['local','gluster']:
         set_storage_type()
@@ -227,6 +265,7 @@ if config['allinone_enable']:
             gluster_server[get_cchostname()] = {'role': 'server'}
             gluster_server_list.append(gluster_server)
             config['glusterfs']['nodes'] = gluster_server_list
+            config['glusterfs']['replica'] = 0
         set_st_nw()
     else:
         print "allinone env storage_type in ['local','gluster']"
@@ -236,7 +275,7 @@ else:
     try:
         hypervisor_type = config['multi_node_hypervisor'].split(',')
     except:
-        print "Please check multi_node_hypervisor in config.sls:  kvm,vmware,ironic"
+        print "ERROR:  check multi_node_hypervisor in config.sls:  kvm,vmware,ironic"
         sys.exit(1)
     if 'kvm' in hypervisor_type:
         config['mg_nw']['hosts']['present'] = get_allhostnameip()
@@ -245,8 +284,11 @@ else:
         config['iaas_role']['cc'] = get_cchostname()
         config['iaas_role']['nn'] = get_cchostname()
     if 'vmware' in hypervisor_type:
-        if len(get_ncnode()) >= 1:
-            config['iaas_role']['vmw_agent'] = get_ncnode()[-1]
+        if get_vmw_agent() in get_allnode().keys():
+            config['iaas_role']['vmw_agent'] = get_vmw_agent()
+        else:
+            print "ERROR vmw_agent not in the online node"
+            sys.exit(1)
     if 'ironic' in hypervisor_type:
         config['ironic_info']['install'] = 'y'
     set_storage_type()
